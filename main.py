5a072ecae8e329fbae262b27aef9c3bc6ce917b0
nullFoo <38643612+nullFoo@users.noreply.github.com>
2021-12-04 11:08:10 +1300

    Only prompt update if changed

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import requests
+
 print("Hello World")
 
+with open("main.py", "r") as f:
+    r = requests.get("https://raw.githubusercontent.com/nullFoo/SelfUpdatingScript/main/main.py")
+    if(f.read !=  r.text):
+        update = input("New update available! Update? (Y/N)")
+        if(update == "Y"):
+            exec(open('updater.py').read())
+            exit()
+
 while(True):
-    update = input("Update? (Y/N)")
-    if(update == "Y"):
-        exec(open('updater.py').read())
+    h = input("type X to exit")
+    if(h == "X"):
         exit()
